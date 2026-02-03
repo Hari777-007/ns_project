365d780c9508ba4857a2de3a7edc9b611d6606e4
Hari777-007 <hariharangk2004@gmail.com>
2026-02-04 01:32:32 +0530

    data validation completed

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from network_security.components.data_ingestion import DataIngestion
+from network_security.components.data_validation import DataValidation
 from network_security.exception.exception import NetworkSecurityException
 from network_security.logging.logger import logging
-from network_security.entity.config_entity import DataIngestionConfig
+from network_security.entity.config_entity import DataIngestionConfig,DataValidationConfig
 from network_security.entity.config_entity import TrainingPipelineConfig
+
  
 
 import sys
@@ -16,7 +18,12 @@ if __name__=='__main__':
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
         logging.info("Data Initiation Completed")
         print(dataingestionartifact)
-       
+        datavalidationconfig=DataValidationConfig(trainingpipelineconfig)
+        data_validation=DataValidation(data_ingestion_artifact=dataingestionartifact, data_validation_config=datavalidationconfig)
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info("Data Validation Completed")
+        print(data_validation_artifact)
+
         
         
         
